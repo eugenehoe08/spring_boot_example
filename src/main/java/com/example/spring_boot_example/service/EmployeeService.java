0b8c7c1fe38b0dd17ee5a1e115afe7c07a61afc0
Eugene <eugenehoe08@hotmail.com>
2022-02-02 22:19:11 +0800

    Use entityManager but leverage native Hibernate API

---
+++ src/main/java/com/example/spring_boot_example/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.example.spring_boot_example.service;
+
+import com.example.spring_boot_example.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> findAll();
+
+    public Employee findById(int theId);
+
+    public void save(Employee theEmployee);
+
+    public void deleteById(int theId);
+}
